cc0a37749bf0b7cf44103a108ccbf92b5b5692a0
Karthik Singireddy <karthsr18@gmail.ciom>
2024-07-04 11:38:06 -0700

    changed to using a browser router

--- app/frontend/src/main.tsx
+++ app/frontend/src/main.tsx
@@ -1,16 +1,22 @@
-import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import App from './app/app';
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <p>test</p>
+  },
+  {
+    path: '/test',
+    element: <p>test path</p>
+  }
+])
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <RouterProvider router={router} />
 );
