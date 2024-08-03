4f33c722c1817e3c18501c398988b4157302fece
Karthik Singireddy <karthsr18@gmail.ciom>
2024-08-03 14:32:09 -0400

    main appshell layout

--- app/frontend/src/routes.tsx
+++ app/frontend/src/routes.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { SignUpPage } from './routes/SignUp.page';
 import { LoginPage } from './routes/Login.page';
+import MainLayout from './routes/main/Layout';
 
 export default createBrowserRouter([
   {
     path: '/',
-    element: <p>test</p>
+    element: <MainLayout />
   },
   {
     path: '/signup',
---
+++ app/frontend/src/routes/main/Layout.tsx
@@ -0,0 +1,48 @@
+import { Outlet } from 'react-router-dom';
+import { AppShell, Burger, Group, Skeleton, useMantineColorScheme } from '@mantine/core';
+import { Simulate } from 'react-dom/test-utils';
+import toggle = Simulate.toggle;
+import { useDisclosure } from '@mantine/hooks';
+import { IconMoon, IconSun } from '@tabler/icons-react';
+
+export default function Layout() {
+  const [opened, { toggle }] = useDisclosure();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened, desktop: opened }
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Group h="100%" px="md" align="center" justify="space-between">
+          <Group>
+            <Burger opened={false} onClick={toggle} size="md" lineSize={1.5}/>
+            <p>calendar assist thing</p>
+          </Group>
+
+          <div>
+            { colorScheme === 'dark' ?
+              <IconMoon cursor='pointer' size={26} onClick={toggleColorScheme} />
+            : <IconSun cursor="pointer" size={28} onClick={toggleColorScheme} />
+            }
+          </div>
+        </Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md" >
+        Navbar
+        {Array(15)
+          .fill(0)
+          .map((_, index) => (
+            <Skeleton key={index} h={28} mt="sm" animate={false} />
+          ))}
+      </AppShell.Navbar>
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  );
+}
