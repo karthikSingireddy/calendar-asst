befac252b1b89b191fd4027c51bf8a988b3a3b2a
Karthik Singireddy <karthsr18@gmail.ciom>
2024-08-07 18:02:40 -0400

    tailwind setup

--- lib/components/src/lib/components.tsx
+++ lib/components/src/lib/components.tsx
@@ -2,7 +2,7 @@ import styles from './components.module.less';
 
 export function Components() {
   return (
-    <div className={styles['container']}>
+    <div className='bg-gray-50'>
       <h1>Welcome to Components!</h1>
     </div>
   );
