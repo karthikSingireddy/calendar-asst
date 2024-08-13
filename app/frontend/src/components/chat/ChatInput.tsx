42effa1dc6e20d3d6153e2e4e47d4ffb0aade47c
Karthik Singireddy <karthsr18@gmail.ciom>
2024-08-13 12:18:39 -0400

    fix ChatInput.tsx

--- app/frontend/src/components/chat/ChatInput.tsx
+++ app/frontend/src/components/chat/ChatInput.tsx
@@ -8,11 +8,11 @@ import { MessageDAO, MessageDTO } from '@calendar-asst/types';
 import {
   Button,
   Label,
-  Textarea,
   Tooltip,
   TooltipContent,
   TooltipProvider,
-  TooltipTrigger
+  TooltipTrigger,
+  Input
 } from '@calendar-asst/components';
 import { CornerDownLeft, Mic, Paperclip } from "lucide-react"
 
@@ -23,26 +23,30 @@ export function ChatInput() {
 
   const sendMessageMutation = useMutation<MessageDAO, DefaultError, MessageDTO>({
     mutationFn: ChatAPI.sendMessage
-  })
+  });
+
+  async function submitMessage() {
+    try {
+      setMessages(oldMessages => [...oldMessages, { content: inputValue, fromUser: true }]);
+      setInputValue('');
+      const responseMessage = await sendMessageMutation.mutateAsync({
+        chatId: chatId,
+        content: inputValue
+      });
+      setMessages(oldMessages => [...oldMessages, { content: responseMessage.content, fromUser: false }]);
+    } catch(e) {
+      console.log(e);
+    }
+  }
 
   async function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === 'Enter' && inputValue.trim().length > 0) {
-      try {
-        setMessages(oldMessages => [...oldMessages, { content: inputValue, fromUser: true }]);
-        setInputValue('');
-        const responseMessage = await sendMessageMutation.mutateAsync({
-          chatId: chatId,
-          content: inputValue
-        });
-        setMessages(oldMessages => [...oldMessages, { content: responseMessage.content, fromUser: false }]);
-      } catch(e) {
-        console.log(e);
-      }
+      await submitMessage();
     }
   }
 
   return (
-    <form
+    <div
       className="m-2 relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
     >
       <TooltipProvider >
@@ -50,10 +54,13 @@ export function ChatInput() {
       <Label htmlFor="message" className="sr-only">
         Message
       </Label>
-      <Textarea
+      <Input
         id="message"
         placeholder="Type your message here..."
         className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <div className="flex items-center p-3 pt-0">
         <Tooltip>
@@ -74,12 +81,12 @@ export function ChatInput() {
           </TooltipTrigger>
           <TooltipContent side="top">Use Microphone</TooltipContent>
         </Tooltip>
-        <Button type="submit" size="sm" className="ml-auto gap-1.5">
+        <Button type="submit" size="sm" className="ml-auto gap-1.5" onClick={submitMessage}>
           Send Message
           <CornerDownLeft className="size-3.5" />
         </Button>
       </div>
       </TooltipProvider>
-    </form>
+    </div>
   );
 }
